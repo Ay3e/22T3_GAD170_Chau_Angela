ead1ded26618d9baa7af229168ccde3c0535260e
Ay3e <angela.chau@students.mq.edu.au>
2022-10-05 11:42:04 +1100

    Week 3 Activities
    Temperature checker & Level Loader

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//
+using UnityEngine.SceneManagement; 
+
+public class LevelLoader : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Loadlevel("WeekTwoActivities");
+        }
+    }
+    /// <summary>
+    /// This will load a different scene.
+    /// </summary>
+    public void Loadlevel(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
---
+++ Assets/Scripts/TemperatureChecker.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureChecker : MonoBehaviour
+{
+    [SerializeField] private float currentPorridgeTemperature = 85.0f;
+
+    private float hotLimitTemperature = 70f;
+    private float coldLimitTemperature = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TemperatureTest();
+        }
+        currentPorridgeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest()
+    {
+        if (currentPorridgeTemperature > hotLimitTemperature)
+        {
+            Debug.Log("The porridge is too hot " + (int)currentPorridgeTemperature + " Degree Celsius");
+        }
+        else if (currentPorridgeTemperature < coldLimitTemperature)
+        {
+            Debug.Log("The porridge is too cold " + (int)currentPorridgeTemperature + " Degree Celsius");
+        }
+        else if (currentPorridgeTemperature == 55f)
+        {
+            Debug.Log("The porridge is exactly 55C" + (int)currentPorridgeTemperature + "Degree Celsius");
+        }
+        else
+        {
+            Debug.Log("The porridge is just right. " + (int)currentPorridgeTemperature + " Degree Celsius");
+        }
+    }
+}
